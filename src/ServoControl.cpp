abe230d94a44ccb2048852b59d98f97686d4650e
subo <gamedevsubo@gmail.com>
2023-03-15 16:41:33 +0100

    Add ServoControls

---
+++ src/ServoControl.cpp
@@ -0,0 +1,33 @@
+#include <Loop/LoopManager.h>
+#include "ServoControl.h"
+
+ServoControl::ServoControl(){
+
+}
+
+void ServoControl::setPos(uint8_t i, uint8_t pos){
+	if(i >= 4) return;
+	pwmValues[i] = constrain(pos, Ranges[i].min, Ranges[i].max);
+	ledcWrite(i, pwmValues[i]);
+}
+
+uint8_t ServoControl::getPos(uint8_t i){
+	if(i >= 4) return 0;
+	return ledcRead(i);
+}
+
+void ServoControl::begin(){
+	for(int i = 0; i < 4; i++){
+		pwmValues[i] = (Ranges[i].min + Ranges[i].max) / 2;
+
+		ledcSetup(i, 200, 8);
+		ledcAttachPin(pwmPins[i], i);
+		ledcWrite(i, pwmValues[i]);
+	}
+}
+
+void ServoControl::end(){
+	for(int i = 0; i < 4; i++){
+		ledcDetachPin(pwmPins[i]);
+	}
+}
---
+++ src/ServoControl.h
@@ -0,0 +1,32 @@
+#ifndef ARMSTRONG_LIBRARY_SERVOCONTROL_H
+#define ARMSTRONG_LIBRARY_SERVOCONTROL_H
+
+
+#include <cstdint>
+#include <Loop/LoopListener.h>
+#include <cstdlib>
+#include "Pins.hpp"
+
+class ServoControl {
+public:
+	ServoControl();
+	void setPos(uint8_t i, uint8_t pos);
+	uint8_t getPos(uint8_t i);
+	void begin();
+	void end();
+
+private:
+	static const uint8_t NumMotors = 4;
+	struct ServoRange{
+		uint8_t min;
+		uint8_t max;
+	};
+	const ServoRange Ranges[NumMotors] = {{ 25, 100 },
+										  { 45, 110 },
+										  { 35, 56 },
+										  { 25, 60 }, };
+	uint8_t pwmValues[4];
+	const uint8_t pwmPins[4] = { SERVO_1, SERVO_2, SERVO_3, SERVO_4 };
+};
+
+#endif //ARMSTRONG_LIBRARY_SERVOCONTROL_H
