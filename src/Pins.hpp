a90399610d4eb8858ea0f6e1956cf47ec23d0aef
subo <gamedevsubo@gmail.com>
2023-03-15 16:40:36 +0100

    Add Pins.hpp

---
+++ src/Pins.hpp
@@ -0,0 +1,29 @@
+#define ENC_1A 34
+#define ENC_1B 39
+#define ENC_2A 16
+#define ENC_2B 4
+#define ENC_3A 19
+#define ENC_3B 18
+#define ENC_4A 5
+#define ENC_4B 17
+
+#define LED_R 23
+#define LED_G 22
+#define LED_B 21
+
+#define SERVO_1 0
+#define SERVO_2 2
+#define SERVO_3 12
+#define SERVO_4 13
+
+#define BTN_1 0
+#define BTN_2 1
+#define BTN_3 2
+#define BTN_4 3
+#define BTN_PP 4
+
+#define LED_1 1
+#define LED_2 2
+#define LED_3 3
+#define LED_4 4
+#define LED_PP 0
