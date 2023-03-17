22ee1f16cb06fa4018b37c3e0ec63d74ceb56173
subo <gamedevsubo@gmail.com>
2023-03-17 13:27:34 +0100

    Add PWMChannels

--- src/ServoControl.cpp
+++ src/ServoControl.cpp
@@ -9,24 +9,24 @@ ServoControl::~ServoControl(){
 	end();
 }
 
-void ServoControl::setPos(uint8_t i, uint8_t pos){
-	if(i >= 4) return;
-	pwmValues[i] = constrain(pos, Ranges[i].min, Ranges[i].max);
-	ledcWrite(i, pwmValues[i]);
+void ServoControl::setPos(uint8_t index, uint8_t pos){
+	if(index >= 4) return;
+	pwmValues[index] = constrain(pos, Ranges[index].min, Ranges[index].max);
+	ledcWrite(PWMChannels[index], pwmValues[index]);
 }
 
-uint8_t ServoControl::getPos(uint8_t i){
-	if(i >= 4) return 0;
-	return ledcRead(i);
+uint8_t ServoControl::getPos(uint8_t index){
+	if(index >= 4) return 0;
+	return ledcRead(PWMChannels[index]);
 }
 
 void ServoControl::begin(){
 	for(int i = 0; i < 4; i++){
 		pwmValues[i] = (Ranges[i].min + Ranges[i].max) / 2;
 
-		ledcSetup(i, 200, 8);
-		ledcAttachPin(pwmPins[i], i);
-		ledcWrite(i, pwmValues[i]);
+		ledcSetup(PWMChannels[i], 200, 8);
+		ledcAttachPin(pwmPins[i], PWMChannels[i]);
+		ledcWrite(PWMChannels[i], pwmValues[i]);
 	}
 }
 
--- src/ServoControl.h
+++ src/ServoControl.h
@@ -11,13 +11,13 @@ class ServoControl {
 public:
 	ServoControl();
 	~ServoControl();
-	void setPos(uint8_t i, uint8_t pos);
-	uint8_t getPos(uint8_t i);
+	void setPos(uint8_t index, uint8_t pos);
+	uint8_t getPos(uint8_t index);
 	void begin();
 	void end();
 
 private:
-	static const uint8_t NumMotors = 4;
+	static constexpr uint8_t NumMotors = 4;
 	struct ServoRange{
 		uint8_t min;
 		uint8_t max;
@@ -26,8 +26,10 @@ private:
 										  { 45, 110 },
 										  { 35, 56 },
 										  { 25, 60 }, };
-	uint8_t pwmValues[4];
-	const uint8_t pwmPins[4] = { SERVO_1, SERVO_2, SERVO_3, SERVO_4 };
+	uint8_t pwmValues[NumMotors];
+	const uint8_t pwmPins[NumMotors] = { SERVO_1, SERVO_2, SERVO_3, SERVO_4 };
+
+	static constexpr uint8_t PWMChannels[NumMotors] = { 0, 1, 2, 3 };
 };
 
 #endif //ARMSTRONG_LIBRARY_SERVOCONTROL_H
